6ae1e95d7d0e54a371ff94ac852463c319968e94
Robert Dyer <rdyer@unl.edu>
2022-05-08 14:29:28 -0500

    add linting, and format current code

--- test/client_test.js
+++ test/client_test.js
@@ -1,19 +1,19 @@
-let BoaClient = require('../lib/boaclient.js').BoaClient
-let BOA_API_ENDPOINT = require('../lib/boaclient.js').BOA_API_ENDPOINT
+const BoaClient = require('../lib/boaclient.js').BoaClient;
+const BOA_API_ENDPOINT = require('../lib/boaclient.js').BOA_API_ENDPOINT;
 
 const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.question('Enter your Boa username: ', function (username) {
-    rl.question('Enter your Boa password: ', function (password) {
-        new BoaClient(BOA_API_ENDPOINT).login(username, password, (client) => {
-            client.datasets()
-        }, (error) => {
-            console.log(error)
-        })
-        rl.close();
+rl.question('Enter your Boa username: ', function(username) {
+  rl.question('Enter your Boa password: ', function(password) {
+    new BoaClient(BOA_API_ENDPOINT+'f').login(username, password, (client) => {
+      client.datasets();
+    }, (error) => {
+      console.log(error);
     });
+    rl.close();
+  });
 });
