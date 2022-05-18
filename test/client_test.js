5e43e1b61261927f372b5d4719b40b5bdecd276e
Robert Dyer <rdyer@unl.edu>
2022-05-18 17:00:03 -0500

    cleanup sample client

--- test/client_test.js
+++ test/client_test.js
@@ -1,41 +1,67 @@
-const boaapi = require('../lib/boaclient.js');
+//
+// Copyright 2022 Robert Dyer,
+//                and University of Nebraska Board of Regents
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 /**
- *
- * @param {*} query
+ * Asks a question on the console and returns their answer.
+ * @param {String} question the question to ask
  * @return {*}
  */
-function ask(query) {
+async function ask(question) {
   const rl = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  return new Promise((resolve) => rl.question(query, (ans) => {
+  return new Promise((resolve) => rl.question(question, (ans) => {
     rl.close();
     resolve(ans);
   }));
 }
 
 /**
- *
+ * The test client main function.
  */
 async function main() {
+  const boaapi = require('../lib/boaclient.js');
+
   const username = await ask('Enter your Boa username: ');
   const password = await ask('Enter your Boa password: ');
 
   const client = new boaapi.BoaClient(boaapi.BOA_API_ENDPOINT);
 
-  await client.login(username, password);
+  client.login(username, password).then(
+      async (result) => {
+        await client.datasets().then(
+            (datasets) => console.log(datasets),
+        );
 
-  await client.datasets().then((datasets) => console.log(datasets));
-  await client.datasets([boaapi.adminFilter,
-    (ds) => typeof ds.id !== 'undefined' ?
-            ds.name.toLowerCase().includes('kotlin') :
-            ds.toLowerCase().includes('kotlin')])
-      .then((datasets) => console.log(datasets));
-
-  await client.close();
+        await client.datasets([boaapi.adminFilter,
+          (ds) => typeof ds.id !== 'undefined' ?
+                  ds.name.toLowerCase().includes('kotlin') :
+                  ds.toLowerCase().includes('kotlin')])
+            .then(
+                (datasets) => console.log(datasets),
+            );
+      },
+  ).finally(
+      async () => {
+        await client.close();
+      },
+  );
 }
 
 main();
